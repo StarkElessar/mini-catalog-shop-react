21d2e7e457dbf0880da768bc51d361ec77aff93a
Stark <serj.elessar@gmail.com>
2022-07-06 02:22:58 +0300

    fix to props

--- src/components/SectionCatalog.jsx
+++ src/components/SectionCatalog.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ProductItems from './ProductItems'
 
-export default function SectionCatalog() {
+export default function SectionCatalog({productData}) {
   return (
     <section className='page__catalog catalog'>
       <div className="catalog__container">
         <h2 className='catalog__title'>Каталог товаров</h2>
 
-        <ProductItems/>
+        <ProductItems items={productData}/>
       </div>
     </section>
   )
