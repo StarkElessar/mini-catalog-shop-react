64dda9b5983b19be55c17dee43fb388ff96de497
Stark <serj.elessar@gmail.com>
2022-07-05 23:53:03 +0300

    changed const date

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { FaRegCopyright } from 'react-icons/fa'
 
 export default function Footer() {
-  const date = new Date()
+  const currentYear = new Date().getFullYear()
 
   return (
     <footer className="footer">
       <div className="footer__container">
         <div className="footer__copyright">
-          <span><FaRegCopyright /> StarkElessar {date.getFullYear()}</span>
+          <span><FaRegCopyright /> StarkElessar {currentYear}</span>
         </div>
       </div>
     </footer>
