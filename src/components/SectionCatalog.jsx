fdc19b14521424114ff45a608f5cb30e5b095438
Stark <serj.elessar@gmail.com>
2022-07-06 00:35:00 +0300

    import Catalog component

--- src/components/SectionCatalog.jsx
+++ src/components/SectionCatalog.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import ProductItems from './ProductItems'
 
 export default function SectionCatalog() {
   return (
     <section className='page__catalog catalog'>
       <div className="catalog__container">
-        <h2 className='catalog__title'>
-          Каталог товаров
-        </h2>
+        <h2 className='catalog__title'>Каталог товаров</h2>
+
+        <ProductItems/>
       </div>
     </section>
   )
