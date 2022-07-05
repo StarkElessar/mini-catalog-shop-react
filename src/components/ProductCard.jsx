15151d2ecc18a3279f814619d3743c0175950cfb
Stark <serj.elessar@gmail.com>
2022-07-06 00:49:40 +0300

    create product-card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function ProductCard() {
   return (
-    <div>ProductCard</div>
+    <article className='catalog__item product-card'>
+      <span className='product-card__id'>код 744.408</span>
+      <img
+        src="https://7745.by/sites/default/files/imagecache/subcat_preview/uploads/products/653011/f2v3gs6w.jpg.webp"
+        alt="Product Image"
+      />
+      <span className='product-card__delivery-type'>Бесплатно по РБ</span>
+      <h4 className='product-card__title'>Стиральная машина BEKO WRE65P2BAA</h4>
+      <span className='product-card__price'>1045,00 р.</span>
+      <span className='product-card__bonus'>Бонус 10,45 р.</span>
+      <button className="product-card__btn">В корзину</button>
+    </article>
   )
 }
