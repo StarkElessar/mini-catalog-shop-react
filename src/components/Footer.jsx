3f7880f90f420753b43ba9ed26f6c79f48fa2f90
Stark <serj.elessar@gmail.com>
2022-07-05 23:51:11 +0300

    footer finished

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { FaRegCopyright } from 'react-icons/fa'
 
 export default function Footer() {
+  const date = new Date()
+
   return (
     <footer className="footer">
       <div className="footer__container">
-
+        <div className="footer__copyright">
+          <span><FaRegCopyright /> StarkElessar {date.getFullYear()}</span>
+        </div>
       </div>
     </footer>
   )
