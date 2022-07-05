3cc0e25c5afdfb2ced38ab755260c0687fa4569e
Stark <serj.elessar@gmail.com>
2022-07-05 23:21:34 +0300

    Finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { FaShoppingBasket } from "react-icons/fa"
 
 export default function Header() {
   return (
-    <header className="hedear">
-      <div className="header__container"></div>
+    <header className="header">
+      <div className="header__container">
+        <div className="header__box">
+          <div className="header__logo">
+            <span>MiniCatalogShop</span>
+          </div>
+          <div className="header__cart cart">
+            <FaShoppingBasket className='cart__icon' />
+            <span className='cart__amount'>0</span>
+          </div>
+        </div>
+      </div>
     </header>
   )
 }
