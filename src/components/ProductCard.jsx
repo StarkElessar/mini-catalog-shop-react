7c907679d4fe871cf281dd0234c46a2636c37d07
Stark <serj.elessar@gmail.com>
2022-07-06 02:23:15 +0300

    fix to props

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import { FaShippingFast } from 'react-icons/fa'
 
-export default function ProductCard() {
+export default function ProductCard(props) {
   return (
     <article className='catalog__item product-card'>
       <div className='product-card__article'>код
-        <span className='product-card__article_id'>744.408</span>
+        <span className='product-card__article_id'>{props.id}</span>
       </div>
       <div className="product-card__img">
-        <img src="https://7745.by/sites/default/files/imagecache/subcat_preview/uploads/products/653011/f2v3gs6w.jpg.webp" alt="Product Image" />
+        <img src={props.imgUrl} alt="Product Image" />
         <div className='product-card__delivery-type'>
           <FaShippingFast className='product-card__delivery-type_icon' />
           <span className='product-card__delivery-type_text'>Бесплатно по РБ</span>
         </div>
       </div>
-      <h4 className='product-card__title'>Стиральная машина BEKO WRE65P2BAA</h4>
-      <span className='product-card__price'>1045,00 р.</span>
-      <span className='product-card__bonus'>Бонус 10,45 р.</span>
+      <h4 className='product-card__title'>{props.title}</h4>
+      <span className='product-card__price'>{props.price.toFixed(2)} р.</span>
+      <span className='product-card__bonus'>Бонус {(props.price / 100).toFixed(2)} р.</span>
       <button className="product-card__btn">В корзину</button>
     </article>
   )
--- src/components/ProductItems.jsx
+++ src/components/ProductItems.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ProductCard from './ProductCard'
 
-export default function ProductItems() {
+export default function ProductItems({items}) {
   return (
     <div className='catalog__wrapper'>
-      <ProductCard />
+      {
+        items.map((item) => {
+          return (
+            <ProductCard
+              key={item.id}
+              id={item.id}
+              imgUrl={item.imgUrl}
+              title={item.title}
+              price={item.price}
+            />
+          )
+        })
+      }
     </div>
   )
 }
