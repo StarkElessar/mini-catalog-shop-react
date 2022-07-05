50cb7fd1fd49ff1a6660db63299dc5b9d331e6e7
Stark <serj.elessar@gmail.com>
2022-07-05 22:20:37 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import React from "react"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+
 function App() {
 
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div className="wrapper">
+      <Header />
+
+      <main className="page">
+
+      </main>
+
+      <Footer />
     </div>
   )
 }
