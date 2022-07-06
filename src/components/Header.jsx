64679b843648d8e3a3038f8f4ad459b32f5704af
Stark <serj.elessar@gmail.com>
2022-07-06 22:12:17 +0300

    add description Logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,12 @@ export default function Header() {
       <div className="header__container">
         <div className="header__box">
           <div className="header__logo">
-            <span>MiniCatalogShop</span>
+            <span className='header__logo_title'>Mini-Catalog Shop</span>
+            <span className='header__logo_desc'>самая доступная техника</span>
           </div>
           <div className="header__cart cart">
+            <span className='cart__cost'>520 Br</span>
+            <i className='cart__divider'></i>
             <FaShoppingBasket className='cart__icon' />
             <span className='cart__amount'>0</span>
           </div>
