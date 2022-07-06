662cdee2650283b9dc00d1cc392ada4097c7f54b
Stark <serj.elessar@gmail.com>
2022-07-06 21:38:01 +0300

    add useSelector()

--- src/components/ProductItems.jsx
+++ src/components/ProductItems.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+
 import ProductCard from './ProductCard'
 
-export default function ProductItems({ items }) {
+export default function ProductItems() {
+  const { items } = useSelector(({ products }) => ({ items: products.items }))
+
   return (
     <div className='catalog__wrapper'>
       {
-        items.map((item) => {
+        items && items.map((item) => {
           return (
             <ProductCard
               key={item.id}
