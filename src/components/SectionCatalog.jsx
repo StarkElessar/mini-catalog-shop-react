54219a656e0adb684d04e619b12ed3baa00d7250
Stark <serj.elessar@gmail.com>
2022-07-06 21:38:18 +0300

    delete items props

--- src/components/SectionCatalog.jsx
+++ src/components/SectionCatalog.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ProductItems from './ProductItems'
 
-export default function SectionCatalog({ productData, onAdd }) {
+export default function SectionCatalog() {
+  
   return (
     <section className='page__catalog catalog'>
       <div className="catalog__container">
         <h2 className='catalog__title'>Каталог товаров</h2>
 
-        <ProductItems items={productData}/>
+        <ProductItems />
       </div>
     </section>
   )
