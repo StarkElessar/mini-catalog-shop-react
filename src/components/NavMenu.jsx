056d42ec90b1d4ad7cd7b900006dd81e9d8eb8e1
Stark <serj.elessar@gmail.com>
2022-07-07 02:12:28 +0300

    create NavMenu logic

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,26 +1,61 @@
 import React from 'react'
 import { FaSortUp } from 'react-icons/fa'
 
-export default function NavMenu() {
+export default function NavMenu({ tags, sortByTags }) {
+  const [activeTag, setActiveTag] = React.useState('все')
+  const [activeSortByTag, setActiveSortByTag] = React.useState('популярности')
+  const [isVisiblePopup, setIsVisiblePopup] = React.useState(false)
+  const sortByRef = React.useRef(null)
+
+  const onSelectTag = (tag) => setActiveTag(tag)
+  const onSelectSortByTag = (tag) => setActiveSortByTag(tag)
+  const setIsVisible = () => setIsVisiblePopup(!isVisiblePopup)
+  const handleOutsideClick = (e) => !e.path.includes(sortByRef.current) && setIsVisiblePopup(false)
+
+  React.useEffect(() => {
+    document.body.addEventListener('click', handleOutsideClick)
+  }, [])
+
   return (
     <nav className='catalog__nav-menu nav-menu'>
       <ul className='nav-menu__list'>
-        <li className='nav-menu__item nav-menu__item_active'>Все</li>
-        <li className='nav-menu__item'>Beko</li>
-        <li className='nav-menu__item'>Schaub</li>
-        <li className='nav-menu__item'>LG</li>
+        {
+          tags.map((tag, index) => {
+            return (
+              <li onClick={() => onSelectTag(tag)} key={`${tag}_${index}`}
+                className={activeTag === tag ? 'nav-menu__item active' : 'nav-menu__item'}
+              >
+                {tag}
+              </li>
+            )
+          })
+        }
       </ul>
-      <div className="nav-menu__sort-by sort-by">
-        <FaSortUp className='sort-by__icon'/>
+      <div ref={sortByRef} onClick={setIsVisible} className="nav-menu__sort-by sort-by">
+        <FaSortUp className={isVisiblePopup ? 'sort-by__icon active' : 'sort-by__icon'} />
         <span className='sort-by__text'>Сортировка по:</span>
-        <span className='sort-by__tag'>популярности</span>
-        <div className="sort-by__popup">
-          <ul className="sort-by__popup_list">
-            <li className="sort-by__popup_item">популярности</li>
-            <li className="sort-by__popup_item">по цене</li>
-            <li className="sort-by__popup_item">по алфавиту</li>
-          </ul>
-        </div>
+        <span className='sort-by__tag'>{activeSortByTag}</span>
+        {
+          isVisiblePopup && (
+            <div className="sort-by__popup">
+              <ul className="sort-by__popup_list">
+                {
+                  sortByTags.map((sortTag, index) => {
+                    return (
+                      <li
+                        onClick={() => onSelectSortByTag(sortTag)}
+                        key={`${sortTag}_${index}`}
+                        className={activeSortByTag === sortTag ? 'sort-by__popup_item active' : 'sort-by__popup_item'}
+                      >
+                        {sortTag}
+                      </li>
+                    )
+                  }) 
+                }
+              </ul>
+            </div>
+          )
+        }
       </div>
     </nav>
   )
--- src/components/SectionCatalog.jsx
+++ src/components/SectionCatalog.jsx
@@ -3,11 +3,15 @@ import NavMenu from './NavMenu'
 import ProductItems from './ProductItems'
 
 export default function SectionCatalog() {
-  
+
   return (
     <section className='page__catalog catalog'>
       <div className="catalog__container">
-        <NavMenu />
+        <NavMenu
+          onClickTag={(tagName) => console.log(tagName)}
+          tags={['все', 'beko', 'schaub', 'lg']}
+          sortByTags={['популярности', 'цене', 'алфавиту']}
+        />
         <ProductItems />
       </div>
     </section>
