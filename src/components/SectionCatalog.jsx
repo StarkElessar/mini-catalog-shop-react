8fcf9a016faea4a8db0a3a43400e2ef018fc7161
Stark <serj.elessar@gmail.com>
2022-07-06 22:12:28 +0300

    delete title

--- src/components/SectionCatalog.jsx
+++ src/components/SectionCatalog.jsx
@@ -6,8 +6,6 @@ export default function SectionCatalog() {
   return (
     <section className='page__catalog catalog'>
       <div className="catalog__container">
-        <h2 className='catalog__title'>Каталог товаров</h2>
-
         <ProductItems />
       </div>
     </section>
