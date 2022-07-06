beb136da1501d180f4f5ff3805bec3136f4c90cc
Stark <serj.elessar@gmail.com>
2022-07-06 04:52:50 +0300

    deleted StrictMode

--- src/index.js
+++ src/index.js
@@ -4,8 +4,4 @@ import './scss/styles.scss'
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+root.render(<App />)
