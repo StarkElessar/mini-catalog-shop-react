f297d01e3e29bc7fd856f4a23a3d7503681b5005
Stark <serj.elessar@gmail.com>
2022-07-07 20:57:10 +0300

    replace axios.get()

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,16 @@
 import React            from 'react'
-import axios            from 'axios'
 import { useDispatch }  from 'react-redux'
 
 import Footer           from './Footer'
 import Header           from './Header'
 import SectionCatalog   from './SectionCatalog'
-import { setProducts }  from '../redux/actions/products'
+import { fetchProducts }  from '../redux/actions/products'
 
 function App() {
   const dispatch = useDispatch()
 
   React.useEffect(() => {
-    axios.get('http://localhost:3000/db.json')
-      .then(({ data }) => dispatch(setProducts(data.products)))
+    dispatch(fetchProducts())
   }, [])
 
 
