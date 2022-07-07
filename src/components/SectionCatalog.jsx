9d5905974dc371d934d33e2c2f5cd7b5611aa16e
Stark <serj.elessar@gmail.com>
2022-07-07 16:05:30 +0300

    replace array for category

--- src/components/SectionCatalog.jsx
+++ src/components/SectionCatalog.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import NavMenu from './NavMenu'
 import ProductItems from './ProductItems'
 
-export default function SectionCatalog() {
+const categoryTags = ['все', 'beko', 'schaub', 'lg']
+const sortTags = ['популярности', 'цене', 'алфавиту']
 
+export default function SectionCatalog() {
   return (
     <section className='page__catalog catalog'>
       <div className="catalog__container">
         <NavMenu
           onClickTag={(tagName) => console.log(tagName)}
-          tags={['все', 'beko', 'schaub', 'lg']}
-          sortByTags={['популярности', 'цене', 'алфавиту']}
+          tags={categoryTags}
+          sortByTags={sortTags}
         />
         <ProductItems />
       </div>
