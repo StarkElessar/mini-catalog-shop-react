f0d59eda4daf76ba3a0f9d41512f8600b681d24c
Stark <serj.elessar@gmail.com>
2022-07-07 20:56:44 +0300

    create action fetchProducs

--- src/redux/actions/products.js
+++ src/redux/actions/products.js
@@ -1,3 +1,11 @@
+import axios from 'axios'
+
+export const fetchProducts = () => (dispatch) => {
+  axios.get('http://localhost:3004/products').then(({ data }) => {
+    dispatch(setProducts(data))
+  })
+}
+
 export const setProducts = (items) => ({
   type: 'SET_PRODUCTS',
   payload: items,
