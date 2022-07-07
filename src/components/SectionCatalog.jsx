37cc9ece1fcf85e0ec6a244734879fb02b29496f
Stark <serj.elessar@gmail.com>
2022-07-07 18:38:18 +0300

    replace two Arrays

--- src/components/SectionCatalog.jsx
+++ src/components/SectionCatalog.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { setCategory, setSortBy } from '../redux/actions/filters'
+
 import NavMenu from './NavMenu'
 import ProductItems from './ProductItems'
 
-const categoryTags = ['все', 'beko', 'schaub', 'lg']
-const sortTags = ['популярности', 'цене', 'алфавиту']
-
 export default function SectionCatalog() {
+  const dispatch = useDispatch()
+  const dispatchSetCategory = React.useCallback((brand) => {
+    dispatch(setCategory(brand))
+  }, [])
+  const dispatchSetSortBy = React.useCallback((type) => {
+    dispatch(setSortBy(type))
+  }, [])
+
   return (
     <section className='page__catalog catalog'>
       <div className="catalog__container">
         <NavMenu
-          onClickTag={(tagName) => console.log(tagName)}
-          tags={categoryTags}
-          sortByTags={sortTags}
+          onClickCategory={dispatchSetCategory}
+          onClickSortBy={dispatchSetSortBy}
         />
         <ProductItems />
       </div>
