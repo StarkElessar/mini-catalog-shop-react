bbc9dfc0f84719c0cbbf2adb4cae0a2315bff6c5
Stark <serj.elessar@gmail.com>
2022-07-07 20:56:25 +0300

    add StrictMode

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import store from './redux/store'
@@ -8,7 +8,9 @@ import App from './components/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
 )
