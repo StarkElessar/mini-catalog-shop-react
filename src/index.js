2288996beec3bbab7326c02e1749c50bdb1cfbf0
Stark <serj.elessar@gmail.com>
2022-07-07 21:30:50 +0300

    init BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React, { StrictMode } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 
 import './scss/styles.scss'
-import App from './components/App'
+import { App } from './components/App'
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <StrictMode>
+  <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
-  </StrictMode>
+  </React.StrictMode>
 )
