51f028c133f273a03cf5594a91386280ce15dc57
Stark <serj.elessar@gmail.com>
2022-07-08 04:03:45 +0300

    skeleton mapping logic

--- src/components/ProductItems.jsx
+++ src/components/ProductItems.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 import ProductCard from './ProductCard'
+import CardSkeleton from './CardSkeleton'
 
 const ProductItems = React.memo(() => {
-  const { items } = useSelector(({ products }) => ({ items: products.items }))
+  const items = useSelector(({ products }) => products.items)
+  const isLoaded = useSelector(({ products }) => products.isLoaded)
 
   return (
     <div className='catalog__wrapper'>
-      { items && items.map((item) => (<ProductCard key={item.id} {...item} /> )) }
+      {
+        isLoaded 
+          ? items.map((item) => <ProductCard key={item.id} {...item} />)
+          : Array(8).fill(0).map((_,index) => <CardSkeleton key={index} />)
+      }
     </div>
   )
 })
