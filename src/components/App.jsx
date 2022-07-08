1effaafcb6c4b611ce1966bef39f2853fb658b3e
Stark <serj.elessar@gmail.com>
2022-07-08 04:02:18 +0300

    rename import for Catalog

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 
 import Footer from './Footer'
 import Header from './Header'
-import SectionCatalog from './SectionCatalog'
+import Catalog from '../pages/Catalog'
+import Cart from '../pages/Cart'
 
 export const App = () => (
   <div className='wrapper'>
     <Header />
     <main className='page'>
-      <SectionCatalog />
+      <Routes>
+        <Route exact path='/' element={<Catalog/>} />
+        <Route exact path='/cart' element={<Cart/>} />
+      </Routes>
     </main>
     <Footer />
   </div>
