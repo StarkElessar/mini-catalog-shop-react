9e4b690a760729cee1689b8901a71ccaafa407a9
Stark <serj.elessar@gmail.com>
2022-07-08 03:59:47 +0300

    create component Cart

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Cart() {
+  return (
+    <section className="page__cart cart">
+      <div className="cart__container">
+        <h2 className='cart__title'>Корзина</h2>
+      </div>
+    </section>
+  )
+}
