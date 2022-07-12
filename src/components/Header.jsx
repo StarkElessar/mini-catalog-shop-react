3336d1a47e6b6e4f63df221d13a56f7be26055e6
Stark <serj.elessar@gmail.com>
2022-07-12 04:05:10 +0300

    replace html to new Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { FaShoppingBasket } from "react-icons/fa"
+
+import Logo from './Logo'
+import ButtonCart from './ButtonCart'
 
 const Header = React.memo(() => (
   <header className="header">
     <div className="header__container">
       <div className="header__box">
-        <Link to='/' className="header__logo">
-          <span className='header__logo_title'>Mini-Catalog Shop</span>
-          <span className='header__logo_desc'>самая доступная техника</span>
-        </Link>
-
-        <Link to='/cart' className="header__cart cart-btn">
-          <span className='cart-btn__cost'>520 Br</span>
-          <i className='cart-btn__divider'></i>
-          <FaShoppingBasket className='cart-btn__icon' />
-          <span className='cart-btn__amount'>0</span>
-        </Link>
+        <Logo/>
+        <ButtonCart/>
       </div>
     </div>
   </header>
