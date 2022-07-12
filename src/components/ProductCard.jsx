45d5bc0b3a96c8c64fc84b6eac13e8930ca6d1bc
Stark <serj.elessar@gmail.com>
2022-07-12 04:05:24 +0300

    fix props

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,26 +2,34 @@ import React from 'react'
 import Delivery from './Delivery'
 
 
-const ProductCard = React.memo(({ ...item }) => {
-  const fixedPrice = item.price.toFixed(2)
-  const bonusCost = (item.price / 100).toFixed(2)
+const ProductCard = React.memo(({ ...objectCard }) => {
+  const {id, title, price, imgUrl, brand, delivery,onClickAddToCart} = {...objectCard}
+  const fixedPrice = price.toFixed(2)
+  const bonusCost = (price / 100).toFixed(2)
+
+  const handleClickToCart = () => onClickAddToCart({id, title, price, imgUrl, brand})
 
   return (
     <article className='catalog__item product-card'>
       <div className='product-card__article'>код
-        <span className='product-card__article_id'>{item.id}</span>
+        <span className='product-card__article_id'>{id}</span>
       </div>
 
       <div className="product-card__img">
-        <img src={item.imgUrl} alt="Product Image" />
-        {item.delivery && <Delivery />}
+        <img src={imgUrl} alt="Product Image" />
+        {delivery && <Delivery />}
       </div>
 
-      <h4 className='product-card__title'>{item.title}</h4>
+      <h4 className='product-card__title'>{title}</h4>
       <span className='product-card__price'>{fixedPrice} р.</span>
       <span className='product-card__bonus'>Бонус {bonusCost} р.</span>
 
-      <button className="product-card__btn">В корзину</button>
+      <button
+        className="product-card__btn"
+        onClick={handleClickToCart}
+      >
+        В корзину
+      </button>
     </article>
   )
 })
