8b0b25b47aa97af4a47f4a4550985aa9b4ea2d9a
Stark <serj.elessar@gmail.com>
2022-07-13 03:32:29 +0300

    refactoring code

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -3,13 +3,11 @@ import React from 'react'
 import Categories from './Categories'
 import SortBy from './SortBy'
 
-const NavMenu = React.memo(({ activeCategory, onClickCategory, activeSortBy, onClickSortBy }) => {
-  return (
-    <nav className='catalog__nav-menu nav-menu'>
-      <Categories activeCategory={activeCategory} onClickCategory={onClickCategory} />
-      <SortBy activeSortBy={activeSortBy} onClickSortBy={onClickSortBy} />
-    </nav>
-  )
-})
+const NavMenu = React.memo(({ activeCategory, onClickCategory, activeSortBy, onClickSortBy }) => (
+  <nav className='catalog__nav-menu nav-menu'>
+    <Categories activeCategory={activeCategory} onClickCategory={onClickCategory} />
+    <SortBy activeSortBy={activeSortBy} onClickSortBy={onClickSortBy} />
+  </nav>
+))
 
 export default NavMenu
