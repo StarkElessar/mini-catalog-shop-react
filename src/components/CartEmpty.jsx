e2455c24172a1fbf72ae1b391d4f1753e2ec3c5c
Stark <serj.elessar@gmail.com>
2022-07-14 02:18:14 +0300

    fix: rename texts

--- src/components/CartEmpty.jsx
+++ src/components/CartEmpty.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import CartEmptyImg from '../images/cart-empty.svg' 
+import cartEmptyImg from '../images/cart-empty.svg' 
 
 function CartEmpty() {
   return (
     <div className="cart__container cart-empty">
-      <h2 className="cart-empty__title">Корзина пустая 😕</h2>
+      <h2 className="cart-empty__title">Корзина пуста 😕</h2>
       <p className="cart-empty__desc">
-        Вероятней всего, вы не заказывали ещё пиццу.
+        Вероятней всего, вы еще не заказывали товары.
       </p>
       <p className="cart-empty__desc">
-        Для того, чтобы заказать пиццу, перейди на главную страницу.
+        Для того, чтобы заказать товар, перейдите на главную страницу.
       </p>
       <div className="cart-empty__img">
-        <img src={CartEmptyImg} alt="Cart Empty"/>
+        <img src={cartEmptyImg} alt="Cart Empty"/>
       </div>
       <Link to='/' className='cart-empty__btn'>Вернуться назад</Link>
     </div>
