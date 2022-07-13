5006439c1b8337bc765c041258943a30b7c1f8d8
Stark <serj.elessar@gmail.com>
2022-07-14 02:17:17 +0300

    create reducers for delete, dec and inc Item in Cart

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -3,8 +3,12 @@ const initialState = {
   totalPrice: 0,
   totalCount: 0
 }
-
-const getTotalPrice = (arr) => arr.reduce((total, {price}) => total + price, 0)
+// Функция для создания единого массива из набора массивов и вложеных обьектов
+const mergedArray = (arr) => (Object.values(arr).map((obj) => obj.items)).flat()
+// Функия для подсчета цены во всех массивах с 2 цифрами после запятой и с типом: число
+const getTotalPrice = (arr) => Number(
+  arr.reduce((totalPrice, { price }) => totalPrice + price , 0).toFixed(2)
+)
 
 const cart = (state = initialState, action) => {
   switch (action.type) {
@@ -19,29 +23,25 @@ const cart = (state = initialState, action) => {
         totalCount: action.payload
       }
     case 'ADD_PRODUCT_TO_CART': {
+      // формирование конкретного товара для добавления в Корзину:
       const currentProductItem = !state.items[action.payload.id]
         ? [action.payload]
         : [...state.items[action.payload.id].items, action.payload]
         
-      const totalPrice = getTotalPrice(currentProductItem).toFixed(2)
-      
-      const newItems = {
+      // Новый обьект добавленных товаров в Корзину:
+      const newObjectItems = {
         ...state.items,
         [action.payload.id]: {
           items: currentProductItem,
-          totalPrice
+          totalPrice: getTotalPrice(currentProductItem)
         }
       }
-      // все товары обьеденённые в один массив:
-      // (или: Object.values(newItems).flat())
-      // (или: [].concat.apply([], Object.values(newItems)))
-      const mergedArray = (Object.values(newItems).map(({items}) => items)).flat()
 
       return {
         ...state,
-        items: newItems,
-        totalCount: mergedArray.length,
-        totalPrice: getTotalPrice(mergedArray).toFixed(2)
+        items: newObjectItems,
+        totalPrice: (getTotalPrice(mergedArray(newObjectItems))),
+        totalCount: mergedArray(newObjectItems).length,
       }
     }
     case 'CLEAR_CART':
@@ -51,6 +51,58 @@ const cart = (state = initialState, action) => {
         totalPrice: 0,
         totalCount: 0
       }
+    case 'REMOVE_CART_ITEM': {
+      const copyCartItems = { ...state.items }
+      delete copyCartItems[action.payload]
+      
+      return { 
+        ...state,
+        items: copyCartItems,
+        totalPrice: getTotalPrice(mergedArray(copyCartItems)),
+        totalCount: mergedArray(copyCartItems).length,
+      }
+    }
+    case 'DECREASE_COUNT_ITEMS': {
+      const currentItems = state.items[action.payload].items
+      const changedItems = currentItems.length > 1
+        ? state.items[action.payload].items.slice(1)
+        : currentItems
+      
+      const newItems = {
+        ...state.items,
+        [action.payload]: {
+          items: changedItems,
+          totalPrice: getTotalPrice(changedItems)
+        }
+      }
+      
+      return {
+        ...state,
+        items: newItems,
+        totalPrice: getTotalPrice(mergedArray(newItems)),
+        totalCount: mergedArray(newItems).length
+      }
+    }
+    case 'INCREASE_COUNT_ITEMS': {
+      const currentItem = [
+        ...state.items[action.payload].items,
+        state.items[action.payload].items[0]
+      ]
+      const newItems = {
+        ...state.items,
+        [action.payload]: {
+          items: currentItem,
+          totalPrice: getTotalPrice(currentItem),
+        }
+      }
+
+      return {
+        ...state,
+        items: newItems,
+        totalPrice: getTotalPrice(mergedArray(newItems)),
+        totalCount: mergedArray(newItems).length
+      }
+    }
     default:
       return state
   }
