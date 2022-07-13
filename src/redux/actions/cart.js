6ac167a0211742d47fb6c1543e6995bc162d5655
Stark <serj.elessar@gmail.com>
2022-07-14 02:16:38 +0300

    create new actions

--- src/redux/actions/cart.js
+++ src/redux/actions/cart.js
@@ -3,4 +3,22 @@ export const addProductToCart = (productObject) => ({
   payload: productObject
 })
 
-export const clearCart = () => ({ type: 'CLEAR_CART' })
+export const clearCart = () => ({
+  type: 'CLEAR_CART'
+})
+
+export const removeCartItem = (id) => ({
+  type: 'REMOVE_CART_ITEM',
+  payload: id
+})
+
+export const decreaseCountItems = (id) => ({
+  type: 'DECREASE_COUNT_ITEMS',
+  payload: id
+
+})
+
+export const increaseCountItems = (id) => ({
+  type: 'INCREASE_COUNT_ITEMS',
+  payload: id
+})
