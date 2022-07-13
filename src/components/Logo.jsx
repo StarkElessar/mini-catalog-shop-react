18bb38214bc416b92674a9a0d8a56970770f399a
Stark <serj.elessar@gmail.com>
2022-07-14 02:16:02 +0300

    fix: link to div

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const Logo = React.memo(() => {
   return (
-    <Link to='/' className="header__logo">
+    <div className="header__logo">
       <span className='header__logo_title'>Mini-Catalog Shop</span>
       <span className='header__logo_desc'>самая доступная техника</span>
-    </Link>
+    </div>
   )
 })
 
